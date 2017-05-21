a46311b3765e2c0ac7acf81c8e1791c2b96a00bb
Francesco Rho <eleven@rhofrances.co>
2017-05-21 23:48:02 +0200

    oweu command step 1

--- app/TelegramCommands/OweUCommand.php
+++ app/TelegramCommands/OweUCommand.php
@@ -2,6 +2,7 @@
 
 use Telegram\Bot\Commands\Command;
 use Telegram\Bot\Keyboard\Keyboard;
+use Telegram;
 
 class OweUCommand extends Command
 {
@@ -19,17 +20,31 @@ class OweUCommand extends Command
   * @inheritdoc
   */
   public function handle($arguments) {
-    //var_dump($arguments);
-    $keyboard = Keyboard::make()
-    ->inline()
-    ->row(
-        Keyboard::inlineButton(['text' => 'Test', 'callback_data' => 'data']),
-        Keyboard::inlineButton(['text' => 'Btn 2', 'callback_data' => 'data_from_btn2'])
-    );
+    // get message
+    $update = $this->getUpdate();
 
-    $this->replyWithMessage([
-      'text' => 'Hello World',
-      'reply_markup' => $keyboard
-    ]);
+    // are we in a group?
+    if($update["message"]["chat"]["type"] === 'group'){
+
+      $keyboard = [
+        [ "\xE2\x9C\x85 Credit", "\xF0\x9F\x94\xB4 Debit"]
+      ];
+
+      $reply_markup = Telegram::replyKeyboardMarkup([
+      	'keyboard' => $keyboard,
+      	'resize_keyboard' => true,
+      	'one_time_keyboard' => true,
+        'selective' => true
+      ]);
+
+      $this->replyWithMessage([
+        'text' => "Do you want to add a credit or debit?",
+        'reply_to_message_id' => $update["message"]["message_id"],
+        'reply_markup' => $reply_markup
+      ]);
+
+    } else {
+      $this->replyWithMessage(['text' => 'OweUBot needs to run in a group to work']);
+    }
   }
 }
