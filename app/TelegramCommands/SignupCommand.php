46cbaa4e7bc2cc6dcdab81010c62598e60e5a334
Francesco Rho <eleven@rhofrances.co>
2017-05-21 23:48:46 +0200

    signup refactor

--- app/TelegramCommands/SignupCommand.php
+++ app/TelegramCommands/SignupCommand.php
@@ -24,17 +24,17 @@ class SignupCommand extends Command
     $this->replyWithChatAction(['action' => Actions::TYPING]);
 
     // get message
-    $user = $this->getUpdate();
+    $update = $this->getUpdate();
 
     // are we in a group?
-    if($user["message"]["chat"]["type"] === 'group'){
+    if($update["message"]["chat"]["type"] === 'group'){
 
       //get fields
       $fields = array();
-      $fields["telegram_user_id"] = $user["message"]["from"]["id"];
-      $fields["telegram_group_id"] = $user["message"]["chat"]["id"];
-      $fields["first_name"] = $user["message"]["from"]["first_name"];
-      $fields["last_name"] = $user["message"]["from"]["last_name"];
+      $fields["telegram_user_id"] = $update["message"]["from"]["id"];
+      $fields["telegram_group_id"] = $update["message"]["chat"]["id"];
+      $fields["first_name"] = $update["message"]["from"]["first_name"];
+      $fields["last_name"] = $update["message"]["from"]["last_name"];
 
       // check if user already exist in the db
       $user = new User;
