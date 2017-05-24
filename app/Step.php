b86d589674d2a0e3827727d75fee3b6c146c7e0c
Francesco Rho <eleven@rhofrances.co>
2017-05-24 23:09:06 +0200

    new steps table

---
+++ app/Step.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OweUBot;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Step extends Model
+{
+    //
+}
---
+++ database/migrations/2017_05_24_165608_create_steps_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('steps', function (Blueprint $table) {
+            $table->increments('id');
+            // telegram reference message id
+            $table->integer('message_id');
+            // step number
+            $table->smallInteger('step');
+            // step 1 - credit / debit
+            $table->boolean('credit');
+            // step 2 - to who
+            $table->integer('to_user_id')->unsigned();
+            $table->foreign('to_user_id')->references('id')->on('users');
+            // user's user id who is using the bot
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('steps');
+    }
+}
